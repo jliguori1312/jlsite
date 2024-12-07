383b98ef26ef5dc81f5a4fab1a0dd86b32af96dc
Jeremy Liguori <jdliguori96@gmail.com>
2024-12-06 18:36:32 -0800

    setup embeds and Image processing (in progress)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jeremyliguori.com',
+    image: {
+        domains: ['admin.jeremyliguori.com']
+    }
 });
