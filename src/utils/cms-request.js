4bbb9f15603df9a0ceeb221c55b267cb3915992f
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-01-22 16:22:25 -0800

    Sort order, pagination, video media

--- src/utils/cms-request.js
+++ src/utils/cms-request.js
@@ -1,10 +1,10 @@
-export const cmsRequest = async (query) => {
+export const cmsRequest = async (query, nextPage) => {
     const domain = import.meta.env.TENANT_DOMAIN
     const endpoint = import.meta.env.ENDPOINT
     //console.log(domain, endpoint)
     //console.log(domain)
 
-    const res = await fetch(`${endpoint}${query}`, {
+    const res = await fetch(`${endpoint}${query}?page=${nextPage}`, {
         method: 'GET',
         headers: {
             'X-Tenant-Domain': domain,
